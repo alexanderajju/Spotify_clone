d20ad2d734b2ecae8da5b50e6e755a8bfd140cce
alexanderajju <alexanderajju@gmail.com>
2020-08-04 09:15:45 +0530

    url shortened and grep the access token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("I have a Token >>>>>>>>>>", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>I am a login</h1> : <Login />}</div>;
 }
 
 export default App;
