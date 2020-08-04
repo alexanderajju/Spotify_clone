9c870ec2d15512a462eb4a20105d3fe9a364d995
alexanderajju <alexanderajju@gmail.com>
2020-08-04 14:54:14 +0530

    Footer  component added

--- src/Player.js
+++ src/Player.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
+import Footer from './Footer'
 
 function Player({ spotify }) {
   return (
-    <div className="player" >
+    <div className="player">
       <div className="player__body">
-        <Sidebar/>
-      <Body/>
+        <Sidebar />
+        <Body />
       </div>
-     
-      {/* footor */}
+
+      <Footer /> 
     </div>
   );
 }
