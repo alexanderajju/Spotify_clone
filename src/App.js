c67d14851b76f64e13a5509525c81a7c5ae08649
alexanderajju <alexanderajju@gmail.com>
2020-08-04 09:19:19 +0530

    added spotifyWebApi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
-
+import spotifyWebApi from "spotify-web-api-js"
 function App() {
   const [token, setToken] = useState(null);
 
@@ -15,7 +15,7 @@ function App() {
       setToken(_token);
     }
 
-    console.log("I have a Token >>>>>>>>>>", token);
+    // console.log("I have a Token >>>>>>>>>>", token);
   }, []);
 
   return <div className="app">{token ? <h1>I am a login</h1> : <Login />}</div>;
