60ff2ebdea5bb0b8763632c3ef9658afad209a88
alexanderajju <alexanderajju@gmail.com>
2020-08-04 14:51:49 +0530

    Body component created

--- src/Player.js
+++ src/Player.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Player.css";
 import Sidebar from "./Sidebar";
+import Body from "./Body";
 
 function Player({ spotify }) {
   return (
     <div className="player" >
       <div className="player__body">
         <Sidebar/>
-      {/* body */}
+      <Body/>
       </div>
      
       {/* footor */}
