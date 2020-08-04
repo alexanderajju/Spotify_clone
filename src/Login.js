6c2551c24e7ea1050c67e4cc14c2fd015fa90f87
alexanderajju <alexanderajju@gmail.com>
2020-08-04 08:22:35 +0530

    Button and logo added

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./Login.css";
 function Login() {
   return (
-    <div className="login" >
-      {/* SPotify Logo */}
-      {/* Login with spotify button */}
-      <h1>Login</h1>
+    <div className="login">
+      <img
+        className="login__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+
+      <button>LOGIN WITH SPOTIFY</button>
     </div>
   );
 }
