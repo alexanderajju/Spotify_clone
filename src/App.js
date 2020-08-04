34fc54103fe05609b2eee9830cc7ecd118b8571e
alexanderajju <alexanderajju@gmail.com>
2020-08-04 08:18:05 +0530

    Login created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="app">
       <h1>Spotify</h1>
-    
+      
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+  return (
+    <div className="login" >
+      {/* SPotify Logo */}
+      {/* Login with spotify button */}
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
